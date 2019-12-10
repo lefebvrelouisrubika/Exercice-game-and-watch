704d82651713f9d68cfe75cf18559cdb94a91862
Louis Lefebvre <l.lefebvre@rubika-edu.com>
2019-12-10 19:53:43 +0100

    added prefab cadeau+code cooldown

---
+++ Exercice Cours de Unity L_L/Assets/LOL_Scripts/CadeauBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CadeauBehaviour : MonoBehaviour
+{
+    private float initCooldown = 0;
+    [SerializeField]
+    private float CooldownLenght;
+    private float Cooldown;
+    public float PositionCadeau;
+    void Start()
+    {
+        Cooldown = initCooldown;
+        PositionCadeau = 1;
+    }
+
+   
+
+    void Update()
+    {
+        if (Cooldown < CooldownLenght)
+        {
+            Cooldown++;
+        }
+        else
+        {
+            Cooldown = initCooldown;
+            PositionCadeau++;
+            Debug.Log("position=" + PositionCadeau);
+        }
+    }
+}
