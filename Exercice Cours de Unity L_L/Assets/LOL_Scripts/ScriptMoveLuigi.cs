1eaf87b7d94f9660fbb70a8cb7cc83b9531ac7e9
Louis Lefebvre <l.lefebvre@rubika-edu.com>
2019-12-10 18:48:42 +0100

    comit suivant: code de mouvements débutés

---
+++ Exercice Cours de Unity L_L/Assets/LOL_Scripts/ScriptMoveLuigi.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptMoveLuigi : MonoBehaviour
+{
+    public float etageLuigi = 1;
+
+
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        MoveMario();
+    }
+
+    void MoveMario()
+    {
+        bool down = Input.GetKeyDown(KeyCode.Z);
+        bool up = Input.GetKeyDown(KeyCode.S);
+        if (down)
+        {
+            if (etageLuigi < 3)
+            {
+                etageLuigi++;
+                Debug.Log("etageluigi=" + etageLuigi);
+            }
+        }
+        if (up)
+        {
+            if (etageLuigi > 1)
+            {
+                etageLuigi--;
+                Debug.Log("etageluigi=" + etageLuigi);
+            }
+        }
+    }
+}
---
+++ Exercice Cours de Unity L_L/Assets/LOL_Scripts/ScriptMoveMario.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptMoveMario : MonoBehaviour
+{
+
+    public float etageMario = 1;
+    
+
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        MoveMario();
+    }
+
+    void MoveMario()
+    {
+        bool down = Input.GetKeyDown(KeyCode.UpArrow);
+        bool up = Input.GetKeyDown(KeyCode.DownArrow);
+        if (down)
+        {
+            if (etageMario < 3)
+            {
+                etageMario++;
+                Debug.Log("etagemario=" + etageMario);
+            }
+        }
+        if (up)
+        {
+            if (etageMario > 1)
+            {
+                etageMario--;
+                Debug.Log("etagemario=" + etageMario);
+            }
+        }
+    }
+}
+
+   
